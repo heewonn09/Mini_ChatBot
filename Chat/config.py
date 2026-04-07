0cb0d8c6a7f2e91cec315c0b7a7b4169e337d959
이희원 <jhjttmtmtmjgt@gmail.com>
2026-04-07 22:47:15 +0900

    feat/Refactor

--- Chat/config.py
+++ Chat/config.py
@@ -1,7 +1,7 @@
-import os
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv(dotenv_path="../.env")  # 🔥 핵심
 
 class Settings:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
